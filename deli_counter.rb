6bdc722c114a3a591470da9bb22167828938b001
Laura Berge <lauraeberge@gmail.com>
2019-09-09 16:21:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,14 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "#{name} is number #{katz_deli.length} in line"
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-  puts 
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    puts "Now serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
 end
