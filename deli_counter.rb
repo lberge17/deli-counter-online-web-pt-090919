2cefbe03e426ee7d1dad4667b53a5ae39befa42d
Laura Berge <lauraeberge@gmail.com>
2019-09-09 16:15:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
-def line(array)
-  if array.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     statement = "The line is currently:"
-    array.each_with_index do |name, index|
+    katz_deli.each_with_index do |name, index|
       statement << " #{index + 1}. #{name}"
     end
     puts statement
   end
 end
+
+def take_a_number(katz_deli, name)
